ed5fdb3a025def72327b1cd01d3a30ee5e8d2435
Emiel de Heij <43185191+emieldatalytica@users.noreply.github.com>
2022-04-11 13:10:38 +0200

    26 code refactoring (#35)
    * delete models folder
    
    * test commit
    
    * add template for pull requests
    
    * remove unused files
    
    * resolve weak linter warnings
    
    * add type hinting
    
    * 28 pr templates (#29) (#30)
    
    * delete models folder
    
    * add template for pull requests
    
    Co-authored-by: KarlijnSchipper <88378830+KarlijnSchipper@users.noreply.github.com>
    Co-authored-by: Emiel de Heij <emiel-de.heij@minbuza.nl>
    
    Co-authored-by: KarlijnSchipper <88378830+KarlijnSchipper@users.noreply.github.com>
    Co-authored-by: Emiel de Heij <emiel-de.heij@minbuza.nl>
    
    * remove notebooks
    
    * add missing type hint
    
    * remove redundant comments
    
    * update patches based on dependabot warnings
    
    * add new change type to PR template
    
    * add comment to ignore file for linter
    
    * refactor top-200 scraper and add precommit hooks
    
    * add mypy configuration and apply black
    
    * update pre-commit hooks, replace tox with pyproject.toml and fix linter errors
    
    * install pflake8 instead of flake8
    
    * add flake8 dependency
    
    Co-authored-by: KarlijnSchipper <88378830+KarlijnSchipper@users.noreply.github.com>
    Co-authored-by: Emiel de Heij <emiel-de.heij@minbuza.nl>

--- spotifAI/models/predict_model.py
+++ spotifAI/models/predict_model.py
@@ -1,23 +1,44 @@
-"""This script loads in the trained model
-and predicts the maximum future position in the
-global spotify top 200 chart for new tracks"""
-import numpy as np
+"""Predict hit potential for tracks using a trained model.
+
+This script loads in the trained model and predicts a ranking
+for the tracks.
+"""
 import pickle
 from tempfile import TemporaryFile
 
+import numpy as np
+import pandas as pd
+from google.cloud import storage
 
-def predict_random(df):
-    """Takes in a dataframe and adds a column 'rank'
-    filled with random values between 1 & 201"""
+
+def predict_random(df: pd.DataFrame) -> pd.DataFrame:
+    """Randomly predicts a ranking for a dataframe with tracks.
+
+    Args:
+        df: a dataframe with tracks to rank
+
+    Returns:
+        object: the dataframe with a column containing a randomly assigned rank
+    """
     df = df.assign(rank=np.random.randint(1, 202, df.shape[0]))
     return df
 
 
-def predict_with_model(df, model_bucket, model_name):
-    """Takes in a dataframe and adds a column 'rank',
-    predicted by a trained classifier that is loaded in from the
-    storage bucket with the provided modelname argument"""
+def predict_with_model(
+    df: pd.DataFrame, model_bucket: storage.bucket.Bucket, model_name: str
+) -> pd.DataFrame:
+    """Predicts a ranking for a dataframe with tracks with a trained model.
 
+    The model is trained on historical data of global top 200 hits.
+
+    Args:
+        df: a dataframe with tracks to rank
+        model_bucket: the cloud bucket containing the trained model
+        model_name: the name of the model to use for predictions
+
+    Returns:
+        object: the dataframe with a column containing the predicted rank
+    """
     full_modelpath = f"models/{model_name}"
 
     blob = model_bucket.blob(full_modelpath)
