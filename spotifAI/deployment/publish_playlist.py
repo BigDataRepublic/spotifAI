ad4f46d7d468987c24222b218704fc01078552d1
Emiel de Heij <emiel-de.heij@minbuza.nl>
2022-10-02 02:59:36 +0200

    cachehandler workaround for authentication without user interaction

--- spotifAI/deployment/publish_playlist.py
+++ spotifAI/deployment/publish_playlist.py
@@ -5,13 +5,47 @@ according to the model's predicted ranking is published via the
 spotify API.
 """
 
+import json
 import logging
 
 import spotipy
 import waitress  # type: ignore
 from flask import Flask, request
 from google.cloud import secretmanager
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import CacheHandler, SpotifyOAuth
+
+
+def access_secret_version(secret_version_id: str) -> str:
+    """Return the value of a secret's version."""
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Access the secret version.
+    response = client.access_secret_version(name=secret_version_id)
+
+    # Return the decoded payload.
+    return response.payload.data.decode("UTF-8")
+
+
+class GoogleCacheHandler(CacheHandler):
+    """Class with custom implementation of Spotipy's CacheHandler.
+
+    This implementation loads and saves the cached access tokens to GCP.
+    """
+
+    def __init__(self) -> None:
+        self.token_info = json.loads(
+            access_secret_version(
+                "projects/420207002838/secrets/DOT-CACHE/versions/latest"
+            )
+        )
+
+    def get_cached_token(self) -> dict:
+        return self.token_info
+
+    def save_token_to_cache(self, token_info: dict) -> None:
+        pass
+        # to implement (?): update dot-cache version with client.update_secret())
 
 
 class SpotifyPlaylistManager:
@@ -21,16 +55,18 @@ class SpotifyPlaylistManager:
         # authenticate to manage playlist
         scope = "playlist-modify-private"
 
-        cid = self.access_secret_version(
+        cid = access_secret_version(
             "projects/420207002838/secrets/SPOTIFY_CLIENT_ID/versions/latest"
         )
 
-        secret = self.access_secret_version(
+        secret = access_secret_version(
             "projects/420207002838/secrets/SPOTIFY_CLIENT_SECRET/versions/latest"
         )
-        red_uri = self.access_secret_version(
+        red_uri = access_secret_version(
             "projects/420207002838/secrets/SPOTIFY_REDIRECT_URI/versions/latest"
         )
+
+        handler = GoogleCacheHandler()
 
         self.sp = spotipy.Spotify(
             auth_manager=SpotifyOAuth(
@@ -38,21 +74,9 @@ class SpotifyPlaylistManager:
                 client_secret=secret,
                 redirect_uri=red_uri,
                 scope=scope,
-                open_browser=False,
+                cache_handler=handler,
             )
         )
-
-    @staticmethod
-    def access_secret_version(secret_version_id: str) -> str:
-        """Return the value of a secret's version."""
-        # Create the Secret Manager client.
-        client = secretmanager.SecretManagerServiceClient()
-
-        # Access the secret version.
-        response = client.access_secret_version(name=secret_version_id)
-
-        # Return the decoded payload.
-        return response.payload.data.decode("UTF-8")
 
     def publish_playlist(self) -> str:
         """Publish the playlist to a personal Spotify account.
